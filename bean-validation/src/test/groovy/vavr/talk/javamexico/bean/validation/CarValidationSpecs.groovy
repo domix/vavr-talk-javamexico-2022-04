e1da1f1c4f5a333b78382cc1057283a95a4a30d5
Domingo Suarez Torres <domingo.suarez@gmail.com>
2022-04-15 11:50:27 -0500

    enhances the test cases

--- bean-validation/src/test/groovy/vavr/talk/javamexico/bean/validation/CarValidationSpecs.groovy
+++ bean-validation/src/test/groovy/vavr/talk/javamexico/bean/validation/CarValidationSpecs.groovy
@@ -5,36 +5,32 @@ import jakarta.validation.Validator
 import spock.lang.Specification
 
 class CarValidationSpecs extends Specification {
-  static Validator validator
+
+  Validator validator
 
   def setup() {
     validator = Validation.buildDefaultValidatorFactory().validator
   }
 
-  def foo() {
+  def 'should validate a Car: "#scenario"'() {
     given:
       def car = Car.builder()
-        .licensePlate("DD-AB-123")
-        .seatCount(4)
+        .manufacturer(manufacturer)
+        .licensePlate(licencePlate)
+        .seatCount(seatCount)
         .build()
     when:
       def constraintViolations = validator.validate(car)
     then:
-      constraintViolations.size() == 1
-      constraintViolations.iterator().next().message == 'must not be null'
-  }
-
-  def bar() {
-    given:
-      def car = Car.builder()
-        .manufacturer("Morris")
-        .licensePlate("D")
-        .seatCount(4)
-        .build()
-    when:
-      def constraintViolations = validator.validate(car)
-    then:
-      constraintViolations.size() == 1
-      constraintViolations.iterator().next().message == 'size must be between 2 and 14'
+      constraintViolations.size() == violationCount
+      if (expectedMessage) {
+        constraintViolations.iterator().next().message == expectedMessage
+      }
+    where:
+      scenario                  | manufacturer | licencePlate | seatCount || violationCount || expectedMessage
+      'car is valid'            | 'Morris'     | 'DD-AB-123'  | 2         || 0              || null
+      'seat count too low'      | 'Morris'     | 'DD-AB-123'  | 1         || 1              || 'must be greater than or equal to 2'
+      'license plate too short' | 'Morris'     | 'D'          | 4         || 1              || 'size must be between 2 and 14'
+      'manufacturer is null'    | null         | 'DD-AB-123'  | 4         || 1              || 'must not be null'
   }
 }
