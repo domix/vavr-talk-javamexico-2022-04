e6af496f45f091fa1b6cf949e486b3d66877ec63
Domingo Suarez Torres <domingo.suarez@gmail.com>
2022-04-15 13:40:57 -0500

    adds javax validation

---
+++ infrastructure/bean-validation/javax/src/main/java/vavr/talk/javamexico/validation/javax/JavaxBeanValidation.java
@@ -0,0 +1,46 @@
+package vavr.talk.javamexico.validation.javax;
+
+import lombok.Builder;
+import lombok.NonNull;
+import vavr.talk.javamexico.validation.BeanValidator;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Collections;
+import java.util.Set;
+
+@Builder
+public class JavaxBeanValidation implements BeanValidator<ConstraintViolation<?>> {
+
+  public static JavaxBeanValidation ofDefault() {
+    return JavaxBeanValidation.builder().build();
+  }
+
+  @Builder.Default
+  private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+  @Nonnull
+  @Override
+  public <T> Set<ConstraintViolation<?>> validate(@Nonnull T object, @Nullable Class<?>... groups) {
+    return Collections.unmodifiableSet(validator.validate(object, groups));
+  }
+
+  @Override
+  public String getMessageCode(@NonNull ConstraintViolation<?> constraintViolation) {
+    return constraintViolation.getMessageTemplate();
+  }
+
+  @Override
+  public String getLocalizedMessage(@NonNull ConstraintViolation<?> constraintViolation) {
+    return constraintViolation.getMessage();
+  }
+
+  @Override
+  public String getPropertyPath(@NonNull ConstraintViolation<?> constraintViolation) {
+    return constraintViolation.getPropertyPath().toString();
+  }
+}
+
