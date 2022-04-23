6472e5e8a6697fdd274aa322600118e2ddfc2aad
Domingo Suarez Torres <domingo.suarez@gmail.com>
2022-04-23 17:37:24 -0500

    some fixes

--- business/persistence/src/main/resources/db/migration/V1.0__investing_entities.sql
+++ business/persistence/src/main/resources/db/migration/V1.0__investing_entities.sql
@@ -9,13 +9,13 @@ CREATE TABLE investing_term
 CREATE TABLE investing_user_interest
 (
     id               BIGSERIAL PRIMARY KEY,
-    user_id          BIGINT     NOT NULL,
-    currency         VARCHAR(3) NOT NULL DEFAULT 'mxn',
-    start_balance    VARCHAR    NOT NULL DEFAULT '0.00',
-    average_balance  VARCHAR    NOT NULL DEFAULT '0.00',
-    end_balance      VARCHAR    NOT NULL DEFAULT '0.00',
-    interest         VARCHAR    NOT NULL DEFAULT '0.00',
-    accrued_interest VARCHAR    NOT NULL DEFAULT '0.00',
-    created          TIMESTAMP  NOT NULL DEFAULT now(),
-    updated          TIMESTAMP  NOT NULL DEFAULT now()
+    user_id          BIGINT      NOT NULL,
+    currency         VARCHAR(10) NOT NULL DEFAULT 'mxn',
+    start_balance    VARCHAR     NOT NULL DEFAULT '0.00',
+    average_balance  VARCHAR     NOT NULL DEFAULT '0.00',
+    end_balance      VARCHAR     NOT NULL DEFAULT '0.00',
+    interest         VARCHAR     NOT NULL DEFAULT '0.00',
+    accrued_interest VARCHAR     NOT NULL DEFAULT '0.00',
+    created          TIMESTAMP   NOT NULL DEFAULT now(),
+    updated          TIMESTAMP   NOT NULL DEFAULT now()
 );
