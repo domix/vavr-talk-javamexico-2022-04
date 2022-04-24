92e40a58b5f748b17e3afea4ed176590a9a69cf7
Domingo Suarez Torres <domingo.suarez@gmail.com>
2022-04-24 12:08:51 -0500

    wip

--- business/persistence/src/main/java/db/migration/V2__AddUsers.java
+++ business/persistence/src/main/java/db/migration/V2__AddUsers.java
@@ -5,7 +5,7 @@ import org.flywaydb.core.api.migration.Context;
 import util.DataUtil;
 
 public class V2__AddUsers extends BaseJavaMigration {
-  public static final int USER_COUNT = 2_000;
+  public static final int USER_COUNT = 2000;
 
   @Override
   public void migrate(Context context) throws Exception {
