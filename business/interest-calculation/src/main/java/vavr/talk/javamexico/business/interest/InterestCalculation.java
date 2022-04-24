ab84e75dfb3774fc32402842a78e791c3c3703de
Domingo Suarez Torres <domingo.suarez@gmail.com>
2022-04-24 00:30:42 -0500

    wip

--- business/interest-calculation/src/main/java/vavr/talk/javamexico/business/interest/InterestCalculation.java
+++ business/interest-calculation/src/main/java/vavr/talk/javamexico/business/interest/InterestCalculation.java
@@ -1,12 +1,10 @@
 package vavr.talk.javamexico.business.interest;
 
-import vavr.talk.javamexico.persistence.jooq.tables.InvestingAccount;
-import vavr.talk.javamexico.persistence.jooq.tables.InvestingContract;
-import vavr.talk.javamexico.persistence.jooq.tables.records.InvestingAccountRecord;
-import vavr.talk.javamexico.persistence.jooq.tables.records.InvestingContractRecord;
+import vavr.talk.javamexico.investing.InvestingAccount;
+import vavr.talk.javamexico.investing.InvestingContract;
 
 import java.math.BigDecimal;
 
 public interface InterestCalculation {
-  BigDecimal interestFor(InvestingAccountRecord account, InvestingContractRecord contract);
+  BigDecimal interestFor(InvestingAccount account, InvestingContract contract);
 }
--- business/interest-calculation/src/main/java/vavr/talk/javamexico/business/interest/impl/DefaultInterestCalculation.java
+++ business/interest-calculation/src/main/java/vavr/talk/javamexico/business/interest/impl/DefaultInterestCalculation.java
@@ -1,16 +1,19 @@
 package vavr.talk.javamexico.business.interest.impl;
 
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.RandomUtils;
 import vavr.talk.javamexico.business.interest.InterestCalculation;
-import vavr.talk.javamexico.persistence.jooq.tables.records.InvestingAccountRecord;
-import vavr.talk.javamexico.persistence.jooq.tables.records.InvestingContractRecord;
+import vavr.talk.javamexico.investing.InvestingAccount;
+import vavr.talk.javamexico.investing.InvestingContract;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class DefaultInterestCalculation implements InterestCalculation {
+  @SneakyThrows
   @Override
-  public BigDecimal interestFor(InvestingAccountRecord account, InvestingContractRecord contract) {
+  public BigDecimal interestFor(InvestingAccount account, InvestingContract contract) {
     BigDecimal bigDecimal = new BigDecimal(contract.getAnnualInterestRate());
 
 
@@ -19,9 +22,12 @@ public class DefaultInterestCalculation implements InterestCalculation {
     int year = calOne.get(Calendar.YEAR);
     Calendar calTwo = new GregorianCalendar(year, 11, 31);
     int day = calTwo.get(Calendar.DAY_OF_YEAR);
-    System.out.println("Days in current year: "+day);
+    System.out.println("Days in current year: " + day);
 
 
-    return null;
+    Thread.sleep(RandomUtils.nextInt(20, 50));
+
+
+    return bigDecimal;
   }
 }
