556f6d6b4882e3cfde49a54d0e6ecff20c3a6b20
Domingo Suarez Torres <domingo.suarez@gmail.com>
2022-04-23 21:13:19 -0500

    inserts contracts a lo pendejo

--- business/persistence/src/main/java/db/migration/V3__AddContracts.java
+++ business/persistence/src/main/java/db/migration/V3__AddContracts.java
@@ -4,16 +4,15 @@ import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import util.DataUtil;
 
-public class V2__AddUsers extends BaseJavaMigration {
+public class V3__AddContracts extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
     DataUtil.populate(
       context,
-      "insert into investing_user (first_name, last_name, email) values %s;",
-      2_000_000,
-      DataUtil::userDataLine
-    );
-  }
+      "insert into investing_contract (contract_name, currency, annual_interest_rate) values %s;",
+      20,
+      DataUtil::contractDataLine);
 
+  }
 }
