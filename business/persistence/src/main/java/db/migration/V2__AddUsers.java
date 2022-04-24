7bc6e8f48ff2dd5e29704a1405b828fca246ac50
Domingo Suarez Torres <domingo.suarez@gmail.com>
2022-04-23 20:27:53 -0500

    adds users a lo pendejo

--- business/persistence/src/main/java/db/migration/V2__AddUsers.java
+++ business/persistence/src/main/java/db/migration/V2__AddUsers.java
@@ -4,7 +4,6 @@ import com.github.javafaker.Faker;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -20,24 +19,19 @@ public class V2__AddUsers extends BaseJavaMigration {
       .collect(Collectors.joining(","));
 
     try (Statement insertStmt = context.getConnection().createStatement()) {
-      try {
-        String sql = "insert into investing_user (first_name, last_name, email) values %s;"
-          .formatted(dataLines);
-        System.out.println("sql: " + sql);
-        insertStmt.executeUpdate(
-          sql
-        );
-      } catch (SQLException e) {
-        throw new RuntimeException(e);
-      }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
+      String massiveInsert = "insert into investing_user (first_name, last_name, email) values %s;"
+        .formatted(dataLines);
+      insertStmt.executeUpdate(massiveInsert);
     }
 
   }
 
   public String dataLine() {
     return "('%s','%s','%s')"
-      .formatted(faker.name().firstName().replace("'", "''"), faker.name().lastName().replace("'", "''"), faker.internet().emailAddress());
+      .formatted(
+        faker.name().firstName().replace("'", "''"),
+        faker.name().lastName().replace("'", "''"),
+        faker.internet().emailAddress()
+      );
   }
 }
