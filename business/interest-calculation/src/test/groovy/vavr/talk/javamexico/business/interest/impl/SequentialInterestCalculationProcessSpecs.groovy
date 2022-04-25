e9d884249efe953decb5f47dc61cef751f8489f3
Domingo Suarez Torres <domingo.suarez@gmail.com>
2022-04-25 00:34:01 -0500

    add some tests

--- business/interest-calculation/src/test/groovy/vavr/talk/javamexico/business/interest/impl/SequentialInterestCalculationProcessSpecs.groovy
+++ business/interest-calculation/src/test/groovy/vavr/talk/javamexico/business/interest/impl/SequentialInterestCalculationProcessSpecs.groovy
@@ -1,7 +1,22 @@
 package vavr.talk.javamexico.business.interest.impl
 
-import spock.lang.Specification
+import org.apache.commons.lang3.time.StopWatch
+import vavr.talk.javamexico.business.interest.InterestCalculation
+import vavr.talk.javamexico.business.interest.InterestCalculationProcess
 
-class SequentialInterestCalculationProcessSpecs extends Specification {
+class SequentialInterestCalculationProcessSpecs extends BaseDBIntegrationSpec {
 
+  def foo() {
+    given:
+      InterestCalculation interestCalculation = new DefaultInterestCalculation(accountRepository, movementRepository)
+      InterestCalculationProcess underTest = new SequentialInterestCalculationProcess(userRepository, contractRepository, interestCalculation)
+      def watch = new StopWatch()
+      watch.start()
+    when:
+      underTest.start()
+      watch.stop()
+    then:
+      underTest
+      println "Tiempo de procesamiento: ${ watch.toString() }"
+  }
 }
