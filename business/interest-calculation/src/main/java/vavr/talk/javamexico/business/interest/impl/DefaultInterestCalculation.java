d72085f3d9f2f698378b23c5ee0dca26d01f6020
Domingo Suarez Torres <domingo.suarez@gmail.com>
2022-04-24 22:52:39 -0500

    working

--- business/interest-calculation/src/main/java/vavr/talk/javamexico/business/interest/impl/DefaultInterestCalculation.java
+++ business/interest-calculation/src/main/java/vavr/talk/javamexico/business/interest/impl/DefaultInterestCalculation.java
@@ -9,17 +9,19 @@ import io.vavr.control.Option;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import vavr.talk.javamexico.Failure;
-import vavr.talk.javamexico.investing.InvestingUser;
 import vavr.talk.javamexico.business.interest.InterestCalculation;
 import vavr.talk.javamexico.business.interest.InterestCalculationContext;
 import vavr.talk.javamexico.investing.InvestingAccount;
 import vavr.talk.javamexico.investing.InvestingContract;
 import vavr.talk.javamexico.investing.InvestingContractMovement;
+import vavr.talk.javamexico.investing.InvestingUser;
 import vavr.talk.javamexico.repository.InvestingAccountRepository;
+import vavr.talk.javamexico.repository.InvestingContractMovementRepository;
 import vavr.talk.javamexico.repository.InvestingUserRepository;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -29,20 +31,63 @@ import java.util.Optional;
 public class DefaultInterestCalculation implements InterestCalculation {
   private final InvestingUserRepository userRepository;
   private final InvestingAccountRepository accountRepository;
-  //private final InvestingContractMovementRepository movementRepository;
+  private final InvestingContractMovementRepository movementRepository;
 
   @Override
   public Optional<Failure> process(InterestCalculationContext context, Long userId) {
     return userRepository.find(userId)
+      .peek(investingUser -> log.info("Usuario encontrado: '{}' ", investingUser.getId()))
       .flatMap(this::calculationDataForUser)
       .map(data -> data.append(context))
       .map(this::calculateInterestFor)
-      .peek(investingContractMovements -> {
+      .peek(movements -> {
+        log.info("Movimientos: {}", movements.size());
+
+        final var accountNewAmounts = new HashMap<Long, BigDecimal>();
+        movements.forEach(data -> {
+          final var account = data._1;
+          final var key = account.getId();
+          final var movement = data._2;
+          BigDecimal amount = movement.getAmount();
+
+          if (accountNewAmounts.containsKey(key)) {
+            amount = amount.add(accountNewAmounts.get(key));
+          }
+          accountNewAmounts.put(key, amount);
+        });
+
+        log.info("data: {}", accountNewAmounts);
+        final var investingAccounts = movements.stream()
+          .map(moves -> moves._1)
+          .filter(account -> accountNewAmounts.containsKey(account.getId()))
+          .map(account -> getBuild(accountNewAmounts, account))
+          .toList();
+
         //guardar datos
-        //accountRepository.updateBatch()
-        //movementRepository.insertBatch(investingContractMovements);
+        accountRepository.updateBatch(investingAccounts).ifPresent(failure -> {
+          log.error(failure.toString());
+        });
+
+        final var investingContractMovements = movements.stream()
+          .map(movs -> movs._2)
+          .toList();
+        movementRepository.insertBatch(investingContractMovements).ifPresent(failure -> {
+          log.error(failure.toString());
+        });
+
       })
       .fold(Optional::of, __ -> Optional.empty());
+  }
+
+  private InvestingAccount getBuild(HashMap<Long, BigDecimal> accountNewAmounts, InvestingAccount account) {
+    return InvestingAccount.builder()
+      .contractId(account.getContractId())
+      .id(account.getId())
+      .currentBalance(account.getCurrentBalance().add(accountNewAmounts.get(account.getId())))
+      .startBalance(account.getStartBalance())
+      .status(account.getStatus())
+      .userId(account.getUserId())
+      .build();
   }
 
   private Either<Failure, Tuple2<InvestingUser, List<InvestingAccount>>> calculationDataForUser(InvestingUser user) {
@@ -51,12 +96,25 @@ public class DefaultInterestCalculation implements InterestCalculation {
   }
 
   private BigDecimal computeInterestFor(InvestingContract contract, InvestingAccount account) {
-    final var mathContext = new MathContext(2);
+    final var template = """
+      Calculando intereses del producto: '{}' para la cuenta '{}'
+         Moneda:  {}
+         APY:     {}
+         Saldo:   {}
+         Balance: {}
+         Interes: {}
+      """;
+
+    final var mathContext = new MathContext(8);
     final var balance = (BigDecimal.ZERO.equals(account.getCurrentBalance())) ?
       account.getStartBalance() : account.getCurrentBalance();
-    return balance
-      .multiply(new BigDecimal(contract.getAnnualInterestRate()), mathContext)
+    final var annualInterestRate = new BigDecimal(contract.getAnnualInterestRate())
+      .divide(new BigDecimal("365"), mathContext);
+    final var divide = balance
+      .multiply(annualInterestRate, mathContext)
       .divide(new BigDecimal("100.00"), mathContext);
+    log.info(template, contract.getContractName(), account.getId(), contract.getCurrency(), contract.getAnnualInterestRate(), account.getCurrentBalance(), balance, divide.toPlainString());
+    return divide;
   }
 
   private Option<InvestingContract> contractFor(InvestingAccount account, InterestCalculationContext context) {
@@ -65,7 +123,7 @@ public class DefaultInterestCalculation implements InterestCalculation {
       .findFirst());
   }
 
-  private List<InvestingContractMovement> calculateInterestFor(Tuple3<InvestingUser, List<InvestingAccount>, InterestCalculationContext> data) {
+  private List<Tuple2<InvestingAccount, InvestingContractMovement>> calculateInterestFor(Tuple3<InvestingUser, List<InvestingAccount>, InterestCalculationContext> data) {
     return data._2.stream()
       .map(account -> createMovementIfNeeded(data, account))
       .map(Value::toJavaOptional)
@@ -74,7 +132,7 @@ public class DefaultInterestCalculation implements InterestCalculation {
       .toList();
   }
 
-  private Option<InvestingContractMovement> createMovementIfNeeded(
+  private Option<Tuple2<InvestingAccount, InvestingContractMovement>> createMovementIfNeeded(
     Tuple3<InvestingUser, List<InvestingAccount>, InterestCalculationContext> data, InvestingAccount account
   ) {
     return contractFor(account, data._3)
@@ -82,11 +140,13 @@ public class DefaultInterestCalculation implements InterestCalculation {
       .map(contract -> createAccountMovement(account, contract));
   }
 
-  private InvestingContractMovement createAccountMovement(InvestingAccount account, InvestingContract contract) {
-    return InvestingContractMovement.builder()
+  private Tuple2<InvestingAccount, InvestingContractMovement> createAccountMovement(InvestingAccount account, InvestingContract contract) {
+    final var interest = InvestingContractMovement.builder()
       .accountId(account.getId())
       .amount(computeInterestFor(contract, account))
       .movementType("interest")
       .build();
+
+    return Tuple.of(account, interest);
   }
 }
--- business/interest-calculation/src/test/groovy/vavr/talk/javamexico/business/interest/impl/DefaultInterestCalculationSpecs.groovy
+++ business/interest-calculation/src/test/groovy/vavr/talk/javamexico/business/interest/impl/DefaultInterestCalculationSpecs.groovy
@@ -4,18 +4,21 @@ import io.vavr.control.Either
 import spock.lang.Specification
 import vavr.talk.javamexico.Failure
 import vavr.talk.javamexico.business.interest.InterestCalculationContext
+import vavr.talk.javamexico.investing.InvestingUser
+import vavr.talk.javamexico.persistence.db.repository.InvestingContractDbRepository
 import vavr.talk.javamexico.repository.InvestingAccountRepository
+import vavr.talk.javamexico.repository.InvestingContractMovementRepository
 import vavr.talk.javamexico.repository.InvestingUserRepository
 
 class DefaultInterestCalculationSpecs extends Specification {
 
-  def foo() {
+  def 'should fail due user is not found'() {
     given:
       def accountRepository = Stub(InvestingAccountRepository)
       def userRepository = Stub(InvestingUserRepository)
-      //userRepository.find(_ as Long) >> Either.right(InvestingUser.builder().id(1l).build())
-      userRepository.find(_ as Long) >> Either.left(Failure.of("", "user not found"))
-      def underTest = new DefaultInterestCalculation(userRepository, accountRepository)
+      userRepository.find(_ as Long) >> Either.left(Failure.of("user not found"))
+      def movementRepository = Stub(InvestingContractMovementRepository)
+      def underTest = new DefaultInterestCalculation(userRepository, accountRepository, movementRepository)
 
       def calculationContext = InterestCalculationContext.builder()
         .contracts([])
@@ -23,7 +26,45 @@ class DefaultInterestCalculationSpecs extends Specification {
     when:
       def interestFor = underTest.process(calculationContext, 1l)
     then:
-      interestFor.isPresent()
+      interestFor.present
       interestFor.get().reason == 'user not found'
   }
+
+  def 'should fail due accountRepository can not retrieve the users accounts'() {
+    given:
+      def accountRepository = Stub(InvestingAccountRepository)
+      def userRepository = Stub(InvestingUserRepository)
+      userRepository.find(_ as Long) >> Either.right(InvestingUser.builder().id(1l).build())
+      accountRepository.findAllActiveAccounts(_ as Long) >> Either.left(Failure.of("No databass"))
+      def movementRepository = Stub(InvestingContractMovementRepository)
+      def underTest = new DefaultInterestCalculation(userRepository, accountRepository, movementRepository)
+
+      def calculationContext = InterestCalculationContext.builder()
+        .contracts([])
+        .build()
+    when:
+      def result = underTest.process(calculationContext, 1L)
+    then:
+      result.present
+  }
+
+  def 'should fail due accountRepository can not retrieve the users accounts 2'() {
+    given:
+      def accountRepository = Stub(InvestingAccountRepository)
+      def userRepository = Stub(InvestingUserRepository)
+      userRepository.find(_ as Long) >> Either.right(InvestingUser.builder().id(1l).build())
+      accountRepository.findAllActiveAccounts(_ as Long) >> Either.right([])
+      def movementRepository = Stub(InvestingContractMovementRepository)
+      def underTest = new DefaultInterestCalculation(userRepository, accountRepository, movementRepository)
+
+      def calculationContext = InterestCalculationContext.builder()
+        .contracts([])
+        .build()
+    when:
+      def result = underTest.process(calculationContext, 1L)
+    then:
+      result.empty
+  }
+
+
 }
